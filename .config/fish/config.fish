917cd7e1403fa89f45794567e4b07e2394555137
yJon4ss <motajoao670@gmail.com>
2022-05-02 10:24:39 +0100

    Update

---
+++ .config/fish/config.fish
@@ -0,0 +1,5 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+alias "vps"="ssh -i "~/Documents/login.pem" admin@ec2-54-234-115-202.compute-1.amazonaws.com"
+alias "arch"="~/ascii/arch"
