d5ed99c1d0286a5bb73b2b96606f115aee0470f0
yJon4ss <motajoao670@gmail.com>
2022-05-11 19:54:58 +0100

    feat: updating polybar config

--- .config/polybar/scripts/polywins/themes/aquarium-light.sh
+++
@@ -1,7 +0,0 @@
-active_text_color="#7D9685"
-active_bg=
-active_underline="#7D9685"
-
-inactive_text_color="#708190"
-inactive_bg=
-inactive_underline=
--- .config/polybar/scripts/polywins/themes/onedark.sh
+++
@@ -1,7 +0,0 @@
-active_text_color="#98c379"
-active_bg=
-active_underline="#98c379"
-
-inactive_text_color="#5c6370"
-inactive_bg=
-inactive_underline=
